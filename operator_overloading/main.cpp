e9c9daf07dd4a7bb08f22025d8956652b221c442
Sergei Sokolov <sokolov12ssa@gmail.com>
2024-10-14 20:58:44 +0500

    comparison op overload

--- operator_overloading/main.cpp
+++ operator_overloading/main.cpp
@@ -1,25 +1,23 @@
-/* Operator overloading in C++ */
+/* Operator overloading in C++ in object-oriented fashion */
 #include <iostream>
 
 class Vector
 {
-    // Member variables/data members
+    // Member variables
 private:
     int m_x{};
     int m_y{};
     int m_z{};
 
 public:
-    // Default constructor
+    // Public default constructor
     Vector() = default;
-    // Regular constructor
+    // Parameterized constructor
     Vector(int x, int y, int z)
         : m_x{x}, m_y{y}, m_z{z}
     {
     }
-    /* Public member functions */
-
-    // Overloading + operator (when adding two objects of the same class together)
+    // Overloading + operator
     Vector operator+(const Vector &rhs) const
     {
         Vector result{};
@@ -28,7 +26,7 @@ public:
         result.m_z = m_z + rhs.m_z;
         return result;
     }
-    // Overloading the increment operator
+    // Overloading the increment operator (++)
     Vector &operator++()
     {
         m_x = m_x + 1;
@@ -36,23 +34,15 @@ public:
         m_z = m_z + 1;
         return *this;
     }
-    // Overloading the cout operator (declaring a friend function inside the class)
-    friend std::ostream &operator<<(std::ostream &out, const Vector &v);
-    // Overloading == operator (declaring as a friend function inside the class)
+    // Overloading the equality operator (bool) (use friend function to access the private members of the class)
     friend bool operator==(const Vector &lhs, const Vector &rhs);
+    // Overloading std::cout operator (use friend funcion in order to access the private members of the class)
+    friend std::ostream &operator<<(std::ostream &out, const Vector &obj);
 };
-
-/* Non-member function definition */
-
-// Overloading the cout operator
-std::ostream &operator<<(std::ostream &out, const Vector &v)
-{
-    out << "Vector: " << v.m_x << " " << v.m_y << " " << v.m_z;
-    return out;
-}
-// Overloading == operator
+// Overloading the equality operator (function definition)
 bool operator==(const Vector &lhs, const Vector &rhs)
 {
+    // Just make a simple comparison between two objects that are passed in here
     if (lhs.m_x == rhs.m_x && lhs.m_y == rhs.m_y && lhs.m_z == rhs.m_z)
     {
         return true;
@@ -60,31 +50,38 @@ bool operator==(const Vector &lhs, const Vector &rhs)
     else
     {
         return false;
-    }
+    };
+}
+// Overloading the cout operator (function implementation)
+std::ostream &operator<<(std::ostream &out, const Vector &obj)
+{
+    out << "The object: X: " << obj.m_x << " Y: " << obj.m_y << " Z: " << obj.m_z << std::endl;
+    return out;
 }
 
 int main(void)
 {
-    // Instantiate class
-    // Vector v1{2, 3, 4};
-    // Vector v2{5, 6, 7};
-    // // Overloading + operator
+    // Instantiate objects from the class
+    // Vector v1{2, 3, 5};
+    // Vector v2{6, 7, 8};
     // v1 = v1 + v2;
-    // std::cout << "Overloading + operator: " << v1.m_x << " " << v1.m_y << " " << v1.m_z << std::endl;
-    // // Overloading the increment operator
-    // Vector v3{5, 10, 12};
+    // // Overloading the plus operator (when adding two objects together)
+    // std::cout << "X: " << v1.m_x << " Y: " << v1.m_y << " Z: " << v1.m_z << std::endl;
+    // // Overloading ++ operator (incrementing an object)
+    // Vector v3{10, 11, 12};
     // ++v3;
-    // std::cout << "Overloading the increment operator: " << v3.m_x << " " << v3.m_y << " " << v3.m_z << std::endl;
-    Vector v4{5, 10, 15};
-    std::cout << v4 << std::endl;
-    Vector v5{5, 0, 15};
-    if (v4 == v5)
+    // std::cout << "X: " << v3.m_x << " Y: " << v3.m_y << " Z: " << v3.m_z << std::endl;
+    Vector v5{1, 2, 3};
+    Vector v6{1, 3, 3};
+    if (v5 == v6)
     {
-        std::cout << "The vectors are equal" << std::endl;
+        std::cout << "The objects are equal" << std::endl;
     }
     else
     {
-        std::cout << "The vectors are not equal" << std::endl;
+        std::cout << "The objects are not equal" << std::endl;
     }
+    Vector v7{10, 15, 25};
+    std::cout << v7;
     return 0;
 }
